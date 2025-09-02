360db77898a4dabb7e06dd56f027049db65e7a81
Chetanchaudhary08 <chetanchaudhary192@gmail.com>
2025-09-03 03:18:14 +0530

    api's error fixed again

--- todo-frontend/src/utils/api.ts
+++ todo-frontend/src/utils/api.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+// Use environment variable (Vite requires VITE_ prefix)
+export const API =
+    import.meta.env.VITE_API_URL || "http://localhost:3000/api/v1";
 
+// Define Todo type
 export interface Todo {
     _id: string;
     title: string;
@@ -10,22 +13,38 @@ export interface Todo {
     completed: boolean;
 }
 
+// ---------- AUTH ----------
+export const loginUser = (data: { email: string; password: string }) =>
+    axios.post<{ token: string; userID: string }>(`${API}/signin`, data);
+
+
+export const registerUser = (data: {
+    username: string;
+    email: string;
+    password: string;
+}) => axios.post<{ message: string }>(`${API}/signup`, data);
+
+// ---------- TODOS ----------
 export const getTodos = (token: string) =>
-    axios.get<Todo[]>(`${API_URL}/todos`, {
+    axios.get<Todo[]>(`${API}/get-todos`, {
         headers: { Authorization: `Bearer ${token}` },
     });
 
-export const createTodo = (token: string, todo: Omit<Todo, "_id">) =>
-    axios.post<Todo>(`${API_URL}/todos`, todo, {
+export const createTodo = (token: string, data: Omit<Todo, "_id">) =>
+    axios.post<Todo>(`${API}/createtodo`, data, {
         headers: { Authorization: `Bearer ${token}` },
     });
 
-export const updateTodo = (token: string, id: string, data: Partial<Todo>) =>
-    axios.put<Todo>(`${API_URL}/todos/${id}`, data, {
+export const updateTodo = (
+    token: string,
+    id: string,
+    data: Partial<Todo>
+) =>
+    axios.put<Todo>(`${API}/update-todo/${id}`, data, {
         headers: { Authorization: `Bearer ${token}` },
     });
 
 export const deleteTodo = (token: string, id: string) =>
-    axios.delete<{ message: string }>(`${API_URL}/todos/${id}`, {
+    axios.delete<{ message: string }>(`${API}/delete-todo/${id}`, {
         headers: { Authorization: `Bearer ${token}` },
     });
